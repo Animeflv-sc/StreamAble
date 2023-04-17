e1068eaf9c5f930e2a5fdfdcade61a343d188338
StreamAble <129634477+Animeflv-sc@users.noreply.github.com>
2023-04-17 13:08:16 -0600

    Update html_gen.py

--- programs/html_gen.py
+++ programs/html_gen.py
@@ -45,9 +45,9 @@ def get_eps_html2(data):
     return html
 
 
-ANIME_POS = """<a href="{}"><div class="poster la-anime"> <div id="shadow1" class="shadow"> <div class="dubb">{}</div><div class="dubb dubb2">{}</div></div><div id="shadow2" class="shadow"> <img class="lzy_img" src="https://cdn.dribbble.com/users/303363/screenshots/2505716/media/51a757785579b2042ede381d9dd1fd82.gif" data-src="{}"> </div><div class="la-details"> <h3>{}</h3> <div id="extra"> <span>{}</span> <span class="dot"></span> <span>{}</span> </div></div></div></a>"""
+ANIME_POS = """<a href="{}"><div class="poster la-anime"> <div id="shadow1" class="shadow"> <div class="dubb">{}</div><div class="dubb dubb2">{}</div></div><div id="shadow2" class="shadow"> <img class="lzy_img" src="https://od.lk/s/OTBfMzYxOTA0MDlf/loader.gif" data-src="{}"> </div><div class="la-details"> <h3>{}</h3> <div id="extra"> <span>{}</span> <span class="dot"></span> <span>{}</span> </div></div></div></a>"""
 
-ANIME_POS2 = """<a href="{}"><div class="poster la-anime"> <div id="shadow1" class="shadow"> <div class="dubb">{}</div></div><div id="shadow2" class="shadow"> <img class="lzy_img" src="https://cdn.dribbble.com/users/303363/screenshots/2505716/media/51a757785579b2042ede381d9dd1fd82.gif" data-src="{}"> </div><div class="la-details"> <h3>{}</h3> <div id="extra"> <span>{}</span> </div></div></div></a>"""
+ANIME_POS2 = """<a href="{}"><div class="poster la-anime"> <div id="shadow1" class="shadow"> <div class="dubb">{}</div></div><div id="shadow2" class="shadow"> <img class="lzy_img" src="https://od.lk/s/OTBfMzYxOTA0MDlf/loader.gif" data-src="{}"> </div><div class="la-details"> <h3>{}</h3> <div id="extra"> <span>{}</span> </div></div></div></a>"""
 
 
 def animeRecHtml(data):
