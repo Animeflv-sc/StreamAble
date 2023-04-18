62c9c6be46d3503cf950ba23830a265b0adaa193
StreamAble <129634477+Animeflv-sc@users.noreply.github.com>
2023-04-17 19:19:12 -0600

    Update script.js

--- static/css/script.js
+++ static/css/script.js
@@ -1,11 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
-import { getDatabase, ref, set, update} from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
 
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js";
+
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
 
 const firebaseConfig = {
-apiKey: "AIzaSyA_-0rmQRfbbBlD1V6v1MzXGnuLkt3VpR4",
+    apiKey: "AIzaSyA_-0rmQRfbbBlD1V6v1MzXGnuLkt3VpR4",
 
     authDomain: "backup-and-restore-79f0c.firebaseapp.com",
 
@@ -26,14 +26,13 @@ apiKey: "AIzaSyA_-0rmQRfbbBlD1V6v1MzXGnuLkt3VpR4",
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
-const database = getDatabase(app);
 
 const submitButton = document.getElementById("submit");
 const signupButton = document.getElementById("sign-up");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const main = document.getElementById("main");
-const createacct = document.getElementById("create-acct");
+const createacct = document.getElementById("create-acct")
 
 const signupEmailIn = document.getElementById("email-signup");
 const confirmSignupEmailIn = document.getElementById("confirm-email-signup");
@@ -44,32 +43,6 @@ const createacctbtn = document.getElementById("create-acct-btn");
 const returnBtn = document.getElementById("return-btn");
 
 var email, password, signupEmail, signupPassword, confirmSignupEmail, confirmSignUpPassword;
-
-// Initialize Firebase Authentication
-const auth = getAuth();
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in
-    const userId = user.uid;
-
-    // Get the data from localStorage
-    const watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
-    const bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
-    const completedList = JSON.parse(localStorage.getItem("completedList")) || [];
-
-    // Store the data in Firebase Realtime database
-    set(ref(database, `users/${userId}`), {
-      email: user.email,
-      watchlist,
-      bookmarks,
-      completedList,
-    }).then(() => {
-      console.log("Data saved to Firebase Realtime Database.");
-    }).catch((error) => {
-      console.error("Error occurred while saving data to Firebase Realtime Database:", error);
-    });
-  }
-});
 
 createacctbtn.addEventListener("click", function() {
   var isVerified = true;
@@ -96,35 +69,41 @@ createacctbtn.addEventListener("click", function() {
   if(isVerified) {
     createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
       .then((userCredential) => {
-        const user = userCredential.user;
-        window.alert("Success! Account created.");
-        window.location.href = "./login.html"; // redirect to homepage
-      })
-      .catch((error) => {
-        console.error("Error occurred while creating user:", error);
-        window.alert("Error occurred while creating user. Try again.");
-      });
+      // Signed in 
+      const user = userCredential.user;
+      // ...
+      window.alert("Success! Account created.");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+      window.alert("Error occurred. Try again.");
+    });
   }
 });
 
 submitButton.addEventListener("click", function() {
   email = emailInput.value;
+  console.log(email);
   password = passwordInput.value;
+  console.log(password);
 
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
+      // Signed in
       const user = userCredential.user;
       console.log("Success! Welcome back!");
-
-      // Redirect to homepage after Firebase Authentication is successful
-      window.location.href = "./settings.html"; 
+      window.alert("Success! Welcome back!");
+      // ...
     })
     .catch((error) => {
-      console.error("Error occurred while signing in:", error);
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("Error occurred. Try again.");
       window.alert("Error occurred. Try again.");
     });
 });
-
 
 signupButton.addEventListener("click", function() {
     main.style.display = "none";
@@ -135,4 +114,3 @@ returnBtn.addEventListener("click", function() {
     main.style.display = "block";
     createacct.style.display = "none";
 });
-
