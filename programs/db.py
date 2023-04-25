8e9c299c41cdb19b4ea14e976487b5a0c9ed39a1
StreamAble <129634477+Animeflv-sc@users.noreply.github.com>
2023-04-25 10:39:02 -0600

    Update db.py

--- programs/db.py
+++ programs/db.py
@@ -1,1 +1,4 @@
-import requests def update_views(anime): try: requests.get("https://api.animedex.live/db/view?anime=" + anime.strip()) except: pass return def update_watch(anime): try: requests.get("https://api.animedex.live/db/watch?anime=" + anime.strip()) except: pass return
+import requests 
+def update_views(anime): try: requests.get("https://api.animedex.live/db/view?anime=" + anime.strip()) 
+except: pass return def update_watch(anime): try: requests.get("https://api.animedex.live/db/watch?anime=" + anime.strip()) 
+except: pass return
